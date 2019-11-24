539c98446f9a1ae79876391cdb32869291504392
Asda <omega-faworit@yandex.ru>
2019-11-24 14:46:35 +0300

    feat: add getting distance and time from HERE REST API

---
+++ databases/here_api.js
@@ -0,0 +1,43 @@
+const fetch = require('node-fetch');
+
+const getLatLon = (city, street, house) => {
+  return new Promise((res, rej) => {
+    fetch(
+      `https://geocoder.api.here.com/6.2/geocode.json?app_id=GiJ0fdprQ8xTIWag6aEp&app_code=3sxT725UJQAD_aB72xmMbg&searchtext=${city}+${street}+${house}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        res(data.Response.View[0].Result[0].Location.DisplayPosition);
+      });
+  });
+};
+
+const getDistance = async addresses => {
+  const coordinates = [];
+  for (let i = 0; i < 2; i++) {
+    coordinates.push(
+      await getLatLon(
+        addresses[i].city,
+        addresses[i].street,
+        addresses[i].house
+      )
+    );
+  }
+  return new Promise((res, rej) => {
+    fetch(
+      `https://route.api.here.com/routing/7.2/calculateroute.json?app_id=GiJ0fdprQ8xTIWag6aEp&app_code=3sxT725UJQAD_aB72xmMbg&waypoint0=geo!${coordinates[0].Latitude},${coordinates[0].Longitude}&waypoint1=geo!${coordinates[1].Latitude},${coordinates[1].Longitude}&mode=fastest;car;traffic:disabled`
+    )
+      .then(res => res.json())
+      .then(data => {
+        //console.log(data.response.route[0].summary);
+        res({
+          distance: data.response.route[0].summary.distance / 1000,
+          time: data.response.route[0].summary.travelTime / 60
+        });
+      });
+  });
+};
+
+module.exports = {
+  getDistance
+};
