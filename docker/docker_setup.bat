20671732a329d1b0e4bf78d210e0b58b079b39cb
Asda <omega-faworit@yandex.ru>
2019-11-30 09:58:20 +0300

    build: add stop containers bat file

--- docker/docker_setup.bat
+++ docker/docker_setup.bat
@@ -1,7 +1,7 @@
 rem docker volume create --name=redisdata
 rem docker run --name redis -d -p 6379:6379 -v redisdata:/data redis
 rem docker volume create --name=neo4jdata
-docker run --name neo4j -d -p 7474:7474 -p 7687:7687 -v neo4jdata:/data -v d:/neo4j_plugins:/plugins -e NEO4J_dbms_security_procedures_unrestricted=apoc.\\\* -e NEO4J_AUTH=neo4j/test neo4j
+rem docker run --name neo4j -d -p 7474:7474 -p 7687:7687 -v neo4jdata:/data -v d:/neo4j_plugins:/plugins -e NEO4J_dbms_security_procedures_unrestricted=apoc.\\\* -e NEO4J_AUTH=neo4j/test neo4j
 rem docker volume create --name=elasticdata
 rem docker run --name elastic -d -p 9200:9200 -p 9300:9300 -v elasticdata:/usr/share/elasticsearch/data -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.4.2
 rem docker network create --driver bridge mongonetwork
@@ -12,4 +12,4 @@ rem docker network create --driver bridge pgnetwork
 rem docker volume create --name=postgresdata
 rem docker run --name postgres -d -p 5432:5432 --hostname=postgres -v=postgres:/pgdata --env-file=pg-env.list --network="pgnetwork" crunchydata/crunchy-postgres:centos7-10.9-2.4.1
 rem docker volume create --name=pgadmindata
-rem docker run --name pgadmin -d -p 5050:5050 -v=pgadmindata:/var/lib/pgadmin --env-file=pgadmin-env.list --network="pgnetwork" crunchydata/crunchy-pgadmin4:centos7-10.9-2.4.1
+docker run --name pgadmin -d -p 5050:5050 -v=pgadmindata:/var/lib/pgadmin --env-file=pgadmin-env.list --network="pgnetwork" crunchydata/crunchy-pgadmin4:centos7-10.9-2.4.1
---
+++ docker/stop.bat
@@ -0,0 +1,4 @@
+docker stop mongo
+docker stop redis
+docker stop postgres
+docker stop neo4j
